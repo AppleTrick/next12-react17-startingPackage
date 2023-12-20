7a3372d186159549293a89903678797e35d9ed0c
ChangHee Park <arrans9591@gmail.com>
2023-12-20 17:36:31 +0900

    eslint 타입스크립트랑 같이 쓰는방법

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,24 @@ module.exports = {
     es2021: true,
   },
   extends: ["standard-with-typescript", "plugin:react/recommended", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest",
+    project: "**/tsconfig.json",
     sourceType: "module",
+    ecmaVersion: "latest",
   },
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/strict-boolean-expressions": "off",
     "@typescript-eslint/no-misused-promises": "off",
     "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "react/display-name": "off",
+    "@typescript-eslint/ban-types": "off",
   },
 };
